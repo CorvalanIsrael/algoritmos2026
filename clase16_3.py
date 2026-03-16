3b0b1f6c222941ce94ce9a6be374f534341d935a
israel <corvalanisraelcb@gmail.com>
2026-03-16 20:28:15 -0300

    clase recursividad

---
+++ clase16_3.py
@@ -0,0 +1,16 @@
+
+def factorial_recursivo (n):
+    if n == 0:
+        return 1
+    else:
+        return n *factorial (n-1)
+    
+def factorial_iterativo (n):
+    result = 1
+    for i in range (1, n+1):
+        result *= i
+
+    return result
+
+input_num = int (input ("Ingrese un numero: "))
+print(factorial_iterativo(input_num))
